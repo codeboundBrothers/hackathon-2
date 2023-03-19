a603b956dab556aedf605cfe5003304aaa95c969
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-03-19 14:32:21 +0530

    data added

---
+++ assets/JS/data.js
@@ -0,0 +1,76 @@
+const articles = {
+    list: [
+        {
+        id: 1,
+        heading: "The Benefits of Exercise",
+        description: "Regular exercise can help you maintain a healthy weight, reduce your risk of chronic diseases, and improve your mood.",
+        author: "John Smith",
+        datePublished: "2022-01-01",
+        timePublished: "12:00:00"
+        },
+        {
+        id: 2,
+        heading: "10 Tips for a Productive Day",
+        description: "Here are some practical tips to help you make the most of your day and increase your productivity.",
+        author: "Jane Doe",
+        datePublished: "2022-02-15",
+        timePublished: "09:30:00"
+        },
+        {
+        id: 3,
+        heading: "How to Create a Successful Blog",
+        description: "Starting a blog can be a great way to share your ideas and connect with others. Here are some tips to help you get started.",
+        author: "David Lee",
+        datePublished: "2022-03-07",
+        timePublished: "16:45:00"
+        },
+        {
+        id: 4,
+        heading: "The Importance of Sleep",
+        description: "Getting enough sleep is essential for your physical and mental health. Here's why and how to improve your sleep habits.",
+        author: "Emily Chen",
+        datePublished: "2022-04-20",
+        timePublished: "22:00:00"
+        },
+        {
+        id: 5,
+        heading: "Healthy Eating on a Budget",
+        description: "Eating healthy doesn't have to be expensive. Here are some tips for eating well while sticking to a budget.",
+        author: "Michael Kim",
+        datePublished: "2022-05-12",
+        timePublished: "11:15:00"
+        },
+        {
+        id: 6,
+        heading: "The Benefits of Meditation",
+        description: "Meditation can help reduce stress, improve focus, and promote emotional well-being. Here's how to get started.",
+        author: "Sarah Jones",
+        datePublished: "2022-06-30",
+        timePublished: "08:00:00"
+        },
+        {
+        id: 7,
+        heading: "How to Manage Your Time Effectively",
+        description: "Time management is key to achieving your goals and reducing stress. Here are some strategies to help you manage your time more effectively.",
+        author: "Mark Tan",
+        datePublished: "2022-07-25",
+        timePublished: "14:20:00"
+        },
+        {
+        id: 8,
+        heading: "Stress Management Techniques",
+        description: "Chronic stress can have negative effects on your health and well-being. Here are some techniques to help you manage stress effectively.",
+        author: "Amy Liu",
+        datePublished: "2022-08-18",
+        timePublished: "17:30:00"
+        },
+        {
+        id: 9,
+        heading: "How to Build Strong Relationships",
+        description: "Healthy relationships are essential for your happiness and well-being. Here are some tips for building and maintaining strong relationships.",
+        author: "Daniel Park",
+        datePublished: "2022-09-03",
+        timePublished: "10:45:00"
+        },
+    ],
+};
